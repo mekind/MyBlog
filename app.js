24225d3b54cd55aa1ac5e835b8c80c524bd455dd
kds <mekind98@gmail.com>
2023-01-02 22:57:58 +0900

    add reactjs

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 80
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.static(path.join(__dirname, 'front/build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front/build/index.html'));
 })
