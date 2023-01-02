a3c1fcce39c777d5d7795687f69ffcfcc18b7959
kds <mekind98@gmail.com>
2023-01-02 22:35:22 +0900

    first setting

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const port = 80
 
-app.use(express.static(__dirname + '/mekind/build'))
-
+// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.send('hello world')
 })
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running : port 3000')
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
